0aec65cfdebb075a73f3e73ab2cbc55c107d2818
PALURU THARUN <153383078+tharun-paluru@users.noreply.github.com>
2024-01-01 18:13:54 +0530

    python program to get the real time data of the weather for a particular location data is getting from by openweathermap

--- weather_app.py
+++ weather_app.py
@@ -4,23 +4,23 @@ import requests
 import json
 import os
 
-api_key = os.getenv("weatherapp_api")
+api_key = os.getenv("weatherapp_api")  #hiding the openweatherapp's APIkey from coders using system varibles
 
 
 #main program
-area = input("Enter the location:")
-url = f"https://api.openweathermap.org/data/2.5/weather?&q={area}&units=metric&appid={api_key}"
+area = input(" Pleasle! Enter the location:")
+url = f"https://api.openweathermap.org/data/2.5/weather?&q={area}&units=metric&appid={api_key}" #url of the openweathermap app to get the real time live data
 weather_data = requests.get(url)
-found=weather_data.status_code
-print(found)
-if found==200:
+found=weather_data.status_code   #to find out weather the entered location is present
+
+if (found==200):                 #Printing the weather conditions in simple form if location is found 
     weather=weather_data.json()["weather"][0]["main"]
     temp = round(weather_data.json()["main"]["temp"])
     humd = round(weather_data.json()["main"]["humidity"])
     print(f"The WEATHER in {area} is :{weather}")
     print(f"The TEMPERATURE in {area} is : {temp}°C")
     print(f"THE HUMIDITY in {area} is: {humd}%")
-elif found==404:
+elif (found==404):
     print("city is not found")
 else:
     print("error occured! please retry")
